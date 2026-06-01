e97ffc320ada801df670f22ab61e8db16c6635c8
drishwillcode <drishtismail+git@gmail.com>
2026-06-01 14:05:18 +0530

    tried login stuff

--- tracker_main.py
+++ tracker_main.py
@@ -1,1 +1,28 @@
-print("check check")
+import pymysql
+#---CREATE A CONNECTION---
+con=pymysql.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    db='v2tracker')
+cur=con.cursor()
+
+#---USER LOGIN---
+print ("welcome")
+print('''1. Login to your account
+    2. create new account''')
+var=int(input("enter your choice"))
+def askusername():
+    un=input("enter username")
+    return un
+def askpwd():
+    pwd=input("enter password")
+    return pwd
+def login():
+    un=askusername
+    
+if var==1:
+elif var==2:    
+        
+        
+
