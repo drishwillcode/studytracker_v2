ef2e89f2db0e48becd12ab80d7e1295817bea800
drishwillcode <drishtismail+git@gmail.com>
2026-06-03 18:57:08 +0530

    there is a bug still AHHHHH

--- tracker_main.py
+++ tracker_main.py
@@ -12,17 +12,26 @@ print ("welcome")
 print('''1. Login to your account
     2. create new account''')
 var=int(input("enter your choice"))
-def askusername():
-    un=input("enter username")
-    return un
-def askpwd():
-    pwd=input("enter password")
-    return pwd
 def login():
-    un=askusername
-    
+    username=input("enter username")
+    pwd=input("enter password")
+    cur.execute("select * from user_info where username={}".format(username))
+    for i in cur.fetchall:
+        if i[2]==pwd:
+            print("login successfull!")
+        else:
+            print("username or password incorrect")
+def signup():
+    username=input("select a username")
+    pwd=input("select a password")
+    query=('''insert into user_info (username,password)
+           values('{}','{}') '''.format(username,pwd))
+    cur.execute(query)
+    print("new account successful!")
 if var==1:
-elif var==2:    
+    login()
+elif var==2:
+    signup()
         
         
 
