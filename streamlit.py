7f1645a7b66a502ba841e56f52775f2909c0e6e9
drishwillcode <drishtismail+git@gmail.com>
2026-07-31 14:32:36 +0530

    new streamlit

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("MY APP")
+st.header("header~~~")
+st.subheader("subheader~~~")
+st.text("some random text blah blah")
+if st.button("click it"):
+    st.write("clock ittttt")
